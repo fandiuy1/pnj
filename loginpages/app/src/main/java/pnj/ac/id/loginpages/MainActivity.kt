9dd85b82f5870a594a68e5ee1df85065489ff108
fandiuy1 <68647079+fandiuy1@users.noreply.github.com>
2024-06-21 20:10:04 +0700

    Add files via upload

---
+++ loginpages/app/src/main/java/pnj/ac/id/loginpages/MainActivity.kt
@@ -0,0 +1,50 @@
+package pnj.ac.id.loginpages
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity() : AppCompatActivity(), Parcelable {
+
+    lateinit var usernameInput : EditText
+    lateinit var passowrdInput : EditText
+    lateinit var loginBtn : Button
+
+    constructor(parcel: Parcel) : this() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        usernameInput = findViewById(R.id.username_input)
+
+
+        }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+}
